a91f9bbfaf2255299ee3f9a24156154733a64307
Baron C <20404222+baron15cao@users.noreply.github.com>
2023-12-27 02:33:35 -0600

    Fixed edge cases of 0/1 trains in API

--- templates/javascript.js
+++ templates/javascript.js
@@ -14,71 +14,87 @@ async function loadSomeDisplay (stationId) {
     
     await fetch(API_URL)
     .then(response => response.json())
-    .then(responseJson => {  
+    .then(responseJson => {
         const mtaRouteText0 = document.getElementById('routeText0');
         const mtaTerminal0 = document.getElementById('terminal0');
         const mtaEta0 = document.getElementById('eta0');
         const mtaRouteText1 = document.getElementById('routeText1');
         const mtaTerminal1 = document.getElementById('terminal1');
         const mtaEta1 = document.getElementById('eta1');
-        mtaRouteText0.innerHTML = responseJson.data[0].alltrains[0].route.charAt(0);
-        mtaTerminal0.innerHTML = responseJson.data[0].alltrains[0].terminalName;
-        mtaEta0.innerHTML = responseJson.data[0].alltrains[0].eta;
-        mtaEta0.innerHTML += ' min';
-        mtaRouteText1.innerHTML = responseJson.data[0].alltrains[1].route.charAt(0);
-        mtaTerminal1.innerHTML = responseJson.data[0].alltrains[1].terminalName;
-        mtaEta1.innerHTML = responseJson.data[0].alltrains[1].eta;
-        mtaEta1.innerHTML += ' min';
-        //for future use to display exp box
-        var svc0 = responseJson.data[0].alltrains[0].service;
-        var svc1 = responseJson.data[0].alltrains[1].service;
-        //to decide if we need a diamond later
         const mtaRoute0 = document.getElementById('route0');
         const mtaRoute1 = document.getElementById('route1');
-        if (responseJson.data[0].alltrains[0].route.slice(-1) == "X")
-        {
-            mtaRoute0.classList.remove('circle');
-            mtaRoute0.classList.add('diamond');
+        var noOfTrains = Object.keys(responseJson.data[0].alltrains).length;
+        var svc0;
+        console.log("Number of trains from API is: ", noOfTrains);
+        if (noOfTrains >=1) {
+            mtaRouteText0.innerHTML = responseJson.data[0].alltrains[0].route.charAt(0);
+            mtaTerminal0.innerHTML = responseJson.data[0].alltrains[0].terminalName;
+            mtaEta0.innerHTML = responseJson.data[0].alltrains[0].eta;
+            mtaEta0.innerHTML += ' min';
+            //for future use to display exp box
+            svc0 = responseJson.data[0].alltrains[0].service;
+            if (responseJson.data[0].alltrains[0].route.slice(-1) == "X")
+            {
+                mtaRoute0.classList.remove('circle');
+                mtaRoute0.classList.add('diamond');
+            }
+            else
+            {
+                mtaRoute0.classList.remove('diamond');
+                mtaRoute0.classList.add('circle');
+            }
         }
-        else
-        {
-            mtaRoute0.classList.remove('diamond');
-            mtaRoute0.classList.add('circle');
+        if (noOfTrains >=2) {
+            mtaRouteText1.innerHTML = responseJson.data[0].alltrains[1].route.charAt(0);
+            mtaTerminal1.innerHTML = responseJson.data[0].alltrains[1].terminalName;
+            mtaEta1.innerHTML = responseJson.data[0].alltrains[1].eta;
+            mtaEta1.innerHTML += ' min';
+            svc1 = responseJson.data[0].alltrains[1].service;
+            if (responseJson.data[0].alltrains[1].route.slice(-1) == "X")
+            {
+                mtaRoute1.classList.remove('circle');
+                mtaRoute1.classList.add('diamond');
+            }
+            else
+            {
+                mtaRoute1.classList.remove('diamond');
+                mtaRoute1.classList.add('circle');
+            }
+        }
+        if (noOfTrains == 0) {
+            mtaRouteText0.innerHTML = "";
+            mtaTerminal0.innerHTML = "No upcoming train";
+            mtaEta0.innerHTML = "";
+            mtaRouteText1.innerHTML = "";
+            mtaTerminal1.innerHTML = "No upcoming train";
+            mtaEta1.innerHTML = "";
         }
 
-        if (responseJson.data[0].alltrains[1].route.slice(-1) == "X")
-        {
-            mtaRoute1.classList.remove('circle');
-            mtaRoute1.classList.add('diamond');
-        }
-        else
-        {
-            mtaRoute1.classList.remove('diamond');
-            mtaRoute1.classList.add('circle');
-        }
-
+        //to decide if we need a diamond later
         const currentDate = new Date();
         const options = { timeZone: 'America/New_York' };
         const currentDateTimeET = currentDate.toLocaleString('en-US', options);     
         document.querySelector('#datetime').textContent = 'ID: ' + stationId + ' ...Station: ' + responseJson.data[0].name + ' ... MTA API Data: ' + responseJson.updated + ' ... Browser Refresh Time in Eastern Time: ' + currentDateTimeET;
     })
+    .catch(err => console.log("Failed at formatting API data. ", err.message))
 }
 
 function runJobOnce() {
     loadSomeDisplay(stationId).then(
         testBlinking => arrivalUpdate()).then(testColoring => routeUpdate()).catch((err) => {
     errorCount += 1;
+    console.log("One error!");
     if (errorCount >= 20) {
         console.log("Too many errors. Abort. Delaying for 15s.");
-        setTimeout(() => {}, "15000");
+        setTimeout(() => {console.log("Time out");}, 15000);
         throw new Error("Something went wrong repeatedly.");
     }
     let userEntry = document.getElementById("stopIdEntry");
     if (userEntry) {
-        userEntry.placeholder = `Invalid station: ${stationId}`;
+        userEntry.placeholder = `Error occured: ${stationId}`;
     }
     stationId = previousStationId;
-    runJobOnce();
+    setTimeout(() => {runJobOnce();}, 2000);
     });
 }
 
@@ -108,14 +124,13 @@ function arrivalUpdate () {
         }
     })
 
-}
+}   
 
 function routeUpdate () {
-let trainrowElements = document.querySelectorAll('.trainrow');
-trainrowElements.forEach(function(trainrowElement) {
+    let trainrowElements = document.querySelectorAll('.trainrow');
+    trainrowElements.forEach(function(trainrowElement) {
     let routeElement = trainrowElement.querySelector('.route');
     let routeValue = routeElement.innerText.charAt(0); 
-
     const routeBackgroundColors = {
         A: '#0039a6',
         C: '#0039a6',
@@ -142,6 +157,9 @@ trainrowElements.forEach(function(trainrowElement) {
         7: '#B933AD'
     }
     let routeBackgroundColor = routeBackgroundColors[routeValue];
+    if (!routeValue) {
+        routeBackgroundColor = '#000000';
+    } 
     let routeTextColor = '#ffffff';
     if (routeValue === 'N' || routeValue === 'Q' || routeValue === 'R' || routeValue === 'W')
     {
@@ -164,7 +182,7 @@ stopForm.addEventListener("submit", (e) => {
   else {
     console.log("User entry: ", userEntry.value);
     previousStationId = stationId;
-    stationId = userEntry.value;
+    stationId = userEntry.value.toUpperCase();
     userEntry.placeholder = `640, 127, 228, 631...`;
   }
   userEntry.value = "";
